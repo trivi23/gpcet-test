05952f239973d5317ab658aee676eada4085e7ee
trivi23 <TRIVIKRAM4567@GMAIL.COM>
2022-03-21 21:19:17 +0530

    assignment-1

---
+++ mqtt_pub.py
@@ -0,0 +1,6 @@
+import paho.mqtt.client as mqtt
+pub_client = mqtt.Client()
+pub_client.connect('broker.hivemq.com',1883)
+print("broker connected")
+
+pub_client.publish('gpcet/ai','bueeeeeeeeeeeeeeeeeeeeeeeeeee')
